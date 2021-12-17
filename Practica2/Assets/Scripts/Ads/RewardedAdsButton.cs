f9caa68c69c4335fd9568f1981cb91b75850e9c5
Nico <nicopast@ucm.es>
2021-12-17 19:05:26 +0100

    Creo que he arreglado los anuncios...

--- Practica2/Assets/Scripts/Ads/RewardedAdsButton.cs
+++ Practica2/Assets/Scripts/Ads/RewardedAdsButton.cs
@@ -75,7 +75,6 @@ public class RewardedAdsButton : MonoBehaviour, IUnityAdsLoadListener, IUnityAds
 
                 rewarded = true;
             }
-
             // Load another ad:
             Advertisement.Load(_adUnitId, this);
         }
